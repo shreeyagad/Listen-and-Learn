98c70d2ba0c84da96165422fc69b5d68396169db
Nicholas Rahardja <nmr73@cornell.edu>
2021-04-05 22:14:23 +0700

    add name

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Listen & Learn - Podcast Recommendation Engine"
-names = ["Kevin Cook: kjc244"]
+names = ["Kevin Cook: kjc244", "Nicholas Rahardja: nmr73"]
 
 
 @irsystem.route("/", methods=["GET"])
