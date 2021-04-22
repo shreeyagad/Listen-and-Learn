e1b48f125ed421000c0f2788d7cc7095749ffb52
Shreeya Gad <shreeyagad@MacBook-Pro-3.local>
2021-04-22 15:29:40 -0400

    changed baseURL back from localhost

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -3,6 +3,5 @@ import axios from "axios";
 export * from "./search";
 
 export default axios.create({
-  // baseURL: "http://listen-and-learn.herokuapp.com/",
-  baseURL: "http://0.0.0.0:5000/",
+  baseURL: "http://listen-and-learn.herokuapp.com/",
 });
